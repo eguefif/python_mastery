56fff3f5ffada0ea3eeb5f88722364e44c4c2d67
Emmanuel Guefif <eguefif@fastmail.com>
2026-01-05 08:47:08 +0100

    decorator_lib: add special functions

--- decorator_library/main.py
+++ decorator_library/main.py
@@ -1,10 +1,25 @@
 import httpx
+
 from decorators import retry
 
 URL = "https://www.scrapethissite.com/pages/"
 
 
-@retry
+class Scraper:
+    def __init__(self, url: str):
+        self.url: str = url
+        self.content: str = ""
+
+    def __repr__(self):
+        return f"Scrapper for: {self.url}"
+
+    def __len__(self):
+        return len(self.content)
+
+    def __getitem__(self, position):
+        return self.content[position]
+
+
 def scrape(url: str):
     r = httpx.get(url)
 
